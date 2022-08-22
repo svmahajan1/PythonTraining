5f5ed58a6b2fcdb733df294b0788029160bac702
svmahajan1 <79442751+svmahajan1@users.noreply.github.com>
2022-08-22 21:33:08 +0530

    Add files via upload

---
+++ Assignment_2.py
@@ -0,0 +1,94 @@
+#Answer 1
+print("\"Learning python\"")
+
+
+#Answer 2
+'''
+This is example
+of Multiline comments
+
+'''
+print()
+
+
+#Answer 3
+a=35
+b=True
+c="MySirG"
+d=5.46
+e=3+4j
+print(a,b,c,d,e)
+print()
+
+
+
+#Answer 4
+a=40
+b=40
+print(id(a))
+print(id(b))
+print()
+
+
+
+
+#Answer 5
+a=5
+b="Sir"
+c=4.5
+d=3+4j
+e=True
+print(a,b,c,d,e)
+print()
+print(type(a),type(b),type(c),type(d),type(e))
+print()
+print(id(a),id(b),id(c),id(d),id(e),sep='\n')
+print()
+
+
+
+#Answer 6
+import keyword
+print(keyword.kwlist)
+print()
+
+
+#Answer 7
+#help()
+#help>keywords
+print()
+
+
+#Answer 8
+# in A0.py, a=369
+# in A1.9y, import A0
+#print(A0.a)
+print()
+
+#Answer 9
+#keywords used as data : True, False, None
+print()
+
+
+#Answer 10
+from datetime import datetime
+dt=datetime.today();
+
+d1=dt.strftime("%d-%m-%Y and %I:%m %p")
+print(d1)
+
+d2=dt.strftime("%B %d %Y")
+print(d2)
+
+d3=dt.strftime("%d%b%Y")
+print(d3)
+
+d4=dt.strftime("%H:%M:%S")
+print(d4)
+
+d5=dt.strftime("%d-%m-%y")
+print(d5)
+
+
+
+
