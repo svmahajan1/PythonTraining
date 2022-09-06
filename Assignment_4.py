13fcdfd7a6faec415602dd21139c5b9e38467d40
svmahajan1 <79442751+svmahajan1@users.noreply.github.com>
2022-09-06 09:47:32 +0530

    Add files via upload

---
+++ Assignment_4.py
@@ -0,0 +1,72 @@
+# Answer 1
+print("Program for  name")
+s=str(input("Enter your name "))
+print(s)
+print()
+
+# Answer 2
+print("Program for number")
+u=int(input("enter a number "))
+print(int(u))
+print()
+
+# Answer 3
+print("Program for sum of two numbers")
+a=int(input("enter first number "))
+b=int(input("enter a second number "))
+c=a+b
+print("sum is", (c))
+print()
+
+
+# Anwer 4
+print("Program for area of circle")
+r=int(input("enter a radius of a circle "))
+A=3.14*r**2
+print("Area of circle is",(A))
+print()
+
+# Anwer 5
+print("Program for square of a number")
+n=int(input("enter a number "))
+print("Square of a number is", n**2)
+print()
+
+# Anwer 6
+print("Program for area of a triangle")
+b=int(input("enter the base of tiangle "))
+h=int(input("enter the heoght of a triangle "))
+print("Area of the Triangle is",0.5*b*h)
+print()
+
+# Answer 7
+print("Program for average of three numbers")
+a1=int(input("enter the 1st number "))
+a2=int(input("enter the 2nd number "))
+a3=int(input("enter the 3rd number "))
+print("Average of three numbers is ",(a1+a2+a3)/3)
+print()
+
+# Answer 8
+print("Program for Simplel interet")
+p=int(input("enter the principal amount "))
+r=int(input("enter the rate of ineterst "))
+t=int(input("enter the teunre" ))
+print("simple intrest is" ,(p*r*t)/100)
+print()
+
+# Answwer 9
+print("program for volume of a cuboid ")
+z=int(input("enter the side of a cubical"))
+print("volume of cuboid" ,z**3)
+print()
+
+# Anwer 10
+print("Program for area of rectanle")
+ll=int(input("enter the side of rectangle "))
+nn=int(input("enter the height  of a rectangle "))
+print("Area of the rectangle is",ll*nn)
+print()
+
+
+
